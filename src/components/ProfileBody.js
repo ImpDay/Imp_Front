a996bc7195018a840678e6da660d6804c5c5d7ba
jihwan <dhwlghksy@gmail.com>
2023-07-19 04:42:33 +0900

    Feat : Make user profile with db data

--- src/components/ProfileBody.js
+++ src/components/ProfileBody.js
@@ -1,64 +1,107 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {useNavigation} from '@react-navigation/native';
 
-
-
-const ProfileBody = ({
-    name,
-    profileImage
-}) => {
-    const navigation=useNavigation();
+const ProfileBody = ({name, profileImage, followingNum, followerNum}) => {
+  const navigation = useNavigation();
+  console.log('------------------');
+  console.log(`This is name : ${name}`);
+  console.log(`This is followingNum : ${followingNum}`);
+  console.log(`This is followerNum : ${followerNum}`);
+  console.log('------------------');
   return (
-        <View>
-            <View style={{position:'relative',flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-around',
-                paddingBottom:'15%'
-            }}>
-                <View style={{paddingLeft:'5%',position:'relative', flexDirection:'row', alignItems:'center', backgroundColor:'black'}}>
-                    <View style={{ position:'relative',alignItems: 'center',}}>
-                        <Text style={{
-                                paddingVertical: '7%', 
-                                fontWeight: 'bold',
-                                fontSize: 25,
-                                color: '#dddddd',
-                            }}>
-                            {name}
-                        </Text>
-                        <Image
-                            source={require('../../images/place/image2.jpeg')}
-                            style={{width: 130, height: 130, borderRadius:90,
-                            }}
-                        />
-
-                    </View>
-                </View>
-                <View style={{paddingLeft:'1%',width:'40%',paddingTop:'25%',position:'relative',flexDirection:'row', alignItems:'center', backgroundColor:'black',}}>
-                    <View style={{posistion:'relative', flexDirection:'column', backgroundColor:'black', alignItems:'center', marginRight:'25%'}}>
-                        <TouchableOpacity onPress={()=>{navigation.push("Follower")}}>
-                            <Text style={{marginLeft:'26%',color:'white', fontWeight:'bold', fontSize:15}}>
-                                61
-                            </Text>
-                            <Text style={{color:'white', fontWeight:'bold', fontSize:16}}>
-                                팔로워
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{position:'relative', flexDirection:'column',}}>
-                        <TouchableOpacity onPress={()=>{navigation.push("Following")}}>
-                            <Text style={{marginLeft:'24%',color:'white', fontWeight:'bold',fontSize:15}}>
-                                83
-                            </Text>
-                            <Text style={{color:'white', fontWeight:'bold',fontSize:16}}>
-                                팔로잉
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
+    <View>
+      <View
+        style={{
+          position: 'relative',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+          paddingBottom: '15%',
+        }}>
+        <View
+          style={{
+            paddingLeft: '5%',
+            position: 'relative',
+            flexDirection: 'row',
+            alignItems: 'center',
+            backgroundColor: 'black',
+          }}>
+          <View style={{position: 'relative', alignItems: 'center'}}>
+            <Text
+              style={{
+                paddingVertical: '7%',
+                fontWeight: 'bold',
+                fontSize: 25,
+                color: '#dddddd',
+              }}>
+              {name}
+            </Text>
+            <Image
+              source={require('../../images/place/image2.jpeg')}
+              style={{width: 130, height: 130, borderRadius: 90}}
+            />
+          </View>
         </View>
-  )
-}
+        <View
+          style={{
+            paddingLeft: '1%',
+            width: '40%',
+            paddingTop: '25%',
+            position: 'relative',
+            flexDirection: 'row',
+            alignItems: 'center',
+            backgroundColor: 'black',
+          }}>
+          <View
+            style={{
+              posistion: 'relative',
+              flexDirection: 'column',
+              backgroundColor: 'black',
+              alignItems: 'center',
+              marginRight: '25%',
+            }}>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.push('Follower');
+              }}>
+              <Text
+                style={{
+                  marginLeft: '26%',
+                  color: 'white',
+                  fontWeight: 'bold',
+                  fontSize: 15,
+                }}>
+                {followerNum}
+              </Text>
+              <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>
+                팔로워
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{position: 'relative', flexDirection: 'column'}}>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.push('Following');
+              }}>
+              <Text
+                style={{
+                  marginLeft: '24%',
+                  color: 'white',
+                  fontWeight: 'bold',
+                  fontSize: 15,
+                }}>
+                {followingNum}
+              </Text>
+              <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>
+                팔로잉
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
 
-export default ProfileBody
+export default ProfileBody;
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,65 +1,90 @@
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
-import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import ProfileBody from '../components/ProfileBody'
-import ProfileButton from '../components/ProfileButton'
-import AntDesign from 'react-native-vector-icons/AntDesign'
-import "./ignore";
+import {View, Text, ScrollView, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import ProfileBody from '../components/ProfileBody';
+import ProfileButton from '../components/ProfileButton';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import './ignore';
+
+class userData {
+  constructor(nickname, follower, following) {
+    this.nickname = nickname;
+    this.followerNum = follower;
+    this.followingNum = following;
+  }
+}
+let userProfile = new userData('Default', '100', '100');
 
 const Profile = ({navigation}) => {
-  const userInfo=[{username:'TankEom', userImage: require('../../images/place/image2.jpeg')}]
-  /*
-  const [userInfo, setUserInfo] = useState({
-    userImage: '',
-    username: '',
-  });
-  useEffect(() => {
-    console.log("실행");
-    // GET 요청을 보내어 데이터를 가져옴
-    fetchUserData = async () => {
-      console.log("요청");
-      try {
-        const response = await Axios.get('');
-        const data = response.data;
-        console.log(data);
-        // 필요한 데이터를 추출하여 user 상태를 업데이트
-        const { userImage, username, travelMap } = data;
-        setUserInfo({
-          userImage,
-          username,
-          travelMap,
-        });
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-      }
-    };
+  const [userProfiles, setuserProfiles] = useState();
+  let nickname;
+  let followingNum;
+  let followerNum;
+  const fetchData = async () => {
+    const url = 'http://172.10.5.148:443/users/nickname';
+    const followingUrl = 'http://172.10.5.148:443/friends/friends';
+    const followerUrl = 'http://172.10.5.148:443/friends/followers';
+    try {
+      const response = await axios.get(url);
+      nickname = response.data.nickname;
+      const followings = await axios.get(followingUrl);
+      const followers = await axios.get(followerUrl);
 
-    fetchUserData();
+      followingNum = followings.data.length;
+      followerNum = followers.data.length;
+      userProfile = new userData(nickname, followerNum, followingNum);
+      console.log(`${nickname} | ${followingNum} | ${followerNum}`);
+      setuserProfiles(userProfile);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
   }, []);
-  */
+
   return (
     <SafeAreaView
-      style={{position:'relative',width:'100%', backgroundColor: 'black', height:'100%'}}>
-        <View style={{position:'relative',flexDirection: 'row', alignItems: 'center', padding: '1%',
-            backgroundColor: 'black',}}>
-            <TouchableOpacity
-                onPress={()=>{
-                navigation.goBack();
-            }}>
-                <AntDesign name="arrowleft" style={{ color:'white', fontSize: 25, paddingTop:'1%', }} /> 
-            </TouchableOpacity>
-        </View>
+      style={{
+        position: 'relative',
+        width: '100%',
+        backgroundColor: 'black',
+        height: '100%',
+      }}>
+      <View
+        style={{
+          position: 'relative',
+          flexDirection: 'row',
+          alignItems: 'center',
+          padding: '1%',
+          backgroundColor: 'black',
+        }}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}>
+          <AntDesign
+            name="arrowleft"
+            style={{color: 'white', fontSize: 25, paddingTop: '1%'}}
+          />
+        </TouchableOpacity>
+      </View>
       <View style={{width: '100%'}}>
         <ProfileBody
-            name={userInfo[0].username}
-            profileImage={userInfo[0].userImage}
+          name={userProfiles?.nickname || 'Loading...'}
+          profileImage={require('../../images/place/image2.jpeg')}
+          followingNum={userProfiles?.followingNum || 0}
+          followerNum={userProfiles?.followerNum || 0}
         />
       </View>
-      <ProfileButton name={userInfo[0].username} profileImage={userInfo[0].userImage}/>
-        
+      <ProfileButton
+        name={userProfiles?.nickname || 'Loading...'}
+        profileImage={require('../../images/place/image2.jpeg')}
+      />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
